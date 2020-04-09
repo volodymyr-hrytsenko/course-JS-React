62a217e53dafba1228322e3e616996041eb6ad3d
volodymyr-hrytsenko <volodyk753@gmail.com>
2020-04-09 17:20:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var leftBorderWidth = 1;
+let seccond = 2;
+const pi = 3.14;
+
+// let answer = confirm("Are you hear?");
+// console.log(answer);
+
+// let answer = +prompt("You are 18 years old?", "Yes");
+// //унарний плюс повертає числове значення
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log("2" == 2);
+console.log("2" === 2); // Перевірка по типах даних
