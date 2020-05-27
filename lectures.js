3d332fdcd4fd1bd03d6c6a75dd2d57c0982d17b5
volodymyr-hrytsenko <volodyk753@gmail.com>
2020-05-27 16:13:35 +0300

    new homework one

--- lectures.js
+++ lectures.js
@@ -3,21 +3,21 @@
 let num = 70;
 
 // тернарна операція
-(num == 50) ? console.log("Вірно") : console.log("Хибно");
+(num == 50) ? console.log("Вірно"): console.log("Хибно");
 
 switch (num) {
-    case num > 80 :
+    case num > 80:
         console.log("Забагато");
-    break;
-    case num < 40 :
+        break;
+    case num < 40:
         console.log("Недостатньо");
-    break;
-    case 50 :
+        break;
+    case 50:
         console.log("Вірно");
-    break;
+        break;
     default:
         console.log("Щось пішло не так");
-    break;
+        break;
 }
 
 /* ------- цикли ------------------------ */
@@ -37,7 +37,7 @@ while (numTwo < 56);
 
 for (let i = 1; i < 8; i++) {
     if (i == 5) {
-        continue;  // пропускаэмо п'яту ітерацію
+        continue; // пропускаэмо п'яту ітерацію
     }
     console.log(i);
 }
@@ -47,13 +47,13 @@ for (let i = 1; i < 8; i++) {
 
 //arrow function
 let add = (a, b) => a + b;
-console.log(add(2,5));
+console.log(add(2, 5));
 
 // function exprission
-let multiply  = function (a, b) {
+let multiply = function (a, b) {
     return (a * b);
 };
-console.log(multiply(2,5));
+console.log(multiply(2, 5));
 
 // function declaration
 function retVar() {
@@ -69,3 +69,70 @@ let twelve = "12.2px";
 // console.log(Math.round(twelve));
 console.log(parseInt(twelve));
 console.log(parseFloat(twelve));
+
+
+/* ------- об'єкти ------------------------- */
+
+let parametrs = {
+    width: 1024,
+    height: 800,
+    color: "red",
+    opasity: 80
+};
+
+for (let key in parametrs) {
+    console.log(key + ": " + parametrs[key] + ";");
+}
+
+delete parametrs.opasity;
+
+console.log(Object.keys(parametrs).length);
+
+/* ------- масиви ------------------------- */
+
+
+let arr = ["one", 3, "four", 5, 7];
+
+arr.forEach(function (item, i, mass) {
+    console.log(i + ": " + item + "(масив: " + mass + ")");
+});
+
+for (let key of arr) {
+    console.log(key);
+}
+
+// let mass = ["apale", "nudels", "souse"],
+//     i = mass.join(", ");
+
+// console.log(i);
+
+// let mass = ["bread", "apale", "souse", "nudels"],
+//     i = mass.sort();
+
+// console.log(i);
+
+let numbers = [10, 3, 14, 5];
+
+numbers.sort(compareNum);
+
+function compareNum(a, b) {
+    return a - b;
+}
+
+console.log(numbers);
+
+/* ------- ООП ------------------------- */
+
+let solder = {
+    health: 400,
+    arme: 100
+};
+
+let john = {
+    health: 100
+}
+
+john.__proto__ = solder;
+
+console.log(john);
+console.log(john.arme);
--- script.js
+++ script.js
@@ -1,84 +1,20 @@
-let money,
-    time;
+let numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
 
-function start () {
-    money = +prompt("Ваш бюджет на месяц?");
-    time = prompt("Введите дату в формате YYYY-MM-DD");
-
-    while (isNaN(money) || money == '' || money == null) {
-        money = +prompt("Ваш бюджет на месяц?");
-    }
-}
-start();
-
-let appData = {
-    budget : money,
-    timesData : time,
-    expenses : {},
-    optionalExpenses : {},
-    income : [],
-    savings : true
+let personalMovieBD = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-function chooseExpenses () {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("Введите обязательную статью расходов в этом месяце"),
-            b = prompt("Во сколько обойдется?");
-        
-        if ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null &&
-            a != '' && b != '' && a.length < 50) {
-            console.log("all right");
-            appData.expenses[a] = b;
-        } else {
-            --i;
-        }
-        
-    }
-}
-// chooseExpenses();
+let num = 0;
 
-function detectDayBudget () {
-    appData.moneyPerDay = (appData.budget / 30).toFixed();
-    alert("Ваш щоденний бюджет = " + appData.moneyPerDay);
+while (num < 2) {
+    const name = prompt('Одинн з фільмів, переглянутих останнім часом?', ''),
+        mark = prompt('Як його оціните?', '');
+    personalMovieBD.movies[name] = mark;
+    num++;
 }
-detectDayBudget();
 
-function detectLevel () {
-    if (appData.moneyPerDay <= 100) {
-        console.log("Мінімальний дохід");
-    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-        console.log("Середній дохід");
-    } else if (appData.moneyPerDay >= 2000) {
-        console.log("Високий дохід");
-    } else {
-        console.log("Сталася помилка");
-    }
-}
-// detectLevel();
-
-function chooseOptExpenses () {
-    for (let i = 1; i <= 3; i++) {
-        let answer = prompt("Статья необязательных расходов?");
-        
-        if ( (typeof(answer)) === "string" && (typeof(answer)) != null &&
-            answer != '' && answer.length < 50) {
-            appData.optionalExpenses[i] = answer;
-        } else {
-            --i;
-        }
-    }
-}
-// chooseOptExpenses();
-
-function checkSavings () {
-    if (appData.savings == true) {
-        let save = +prompt("Яка сума збережень?"),
-            percent = +prompt("Під який відсоток?");
-        
-        appData.monthIncome = save/100/12*percent;
-        alert("Дохід в місяць з вашого депозиту: " + appData.monthIncome);
-    }
-}
-checkSavings();
-
-console.log(appData);
+console.log(personalMovieBD);
