a9dffe969fa476614d77acae5676edc17428b84d
volodymyr-hrytsenko <volodyk753@gmail.com>
2020-05-29 13:29:53 +0300

    homework three

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+let numberOfFilms;
 
-let personalMovieBD = {
+function start() {
+    numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+    }
+}
+
+start();
+
+const personalMovieBD = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -8,24 +18,46 @@ let personalMovieBD = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const name = prompt('Одинн з фільмів, переглянутих останнім часом?', ''),
-        mark = prompt('Як його оціните?', '');
-    if (name !== null && mark !== null && name != '' && mark != '' && name.length < 50) {
-        personalMovieBD.movies[name] = mark;
-    } else {
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const name = prompt('Одинн з фільмів, переглянутих останнім часом?', ''),
+            mark = prompt('Як його оціните?', '');
+        if (name !== null && mark !== null && name != '' && mark != '' && name.length < 50) {
+            personalMovieBD.movies[name] = mark;
+        } else {
+            i--;
+        }
     }
 }
 
-if (personalMovieBD.count < 10) {
-    alert('Переглянуто досить мало фільмів');
-} else if (personalMovieBD.count >= 10 && personalMovieBD.count < 30) {
-    alert('Ви класичний глядач');
-} else if (personalMovieBD.count >= 30) {
-    alert('Ви кіноман');
-} else {
-    alert('Сталася помилка');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieBD.count < 10) {
+        alert('Переглянуто досить мало фільмів');
+    } else if (personalMovieBD.count >= 10 && personalMovieBD.count < 30) {
+        alert('Ви класичний глядач');
+    } else if (personalMovieBD.count >= 30) {
+        alert('Ви кіноман');
+    } else {
+        alert('Сталася помилка');
+    }
 }
 
-console.log(personalMovieBD);
+detectPersonalLevel();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieBD.genres.push(prompt(`Ваш улюблений жанр номером ${i}`, ''));
+    }
+}
+
+writeYourGenres();
+
+function showMyBD(hidden) {
+    if (!hidden) {
+        console.log(personalMovieBD);
+    }
+}
+
+showMyBD(personalMovieBD.privat);
